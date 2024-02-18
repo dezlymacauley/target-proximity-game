fb1e0d9e0830503dff34ba353a8bdf563b839292
dezlymacauley <dezlymacauley@proton.me>
2024-02-18 15:21:17 +0200

    Added a function to collect user input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+// The io module in Rust provides functionality for input and output operations,
+// including reading from and writing to files,
+// handling standard input and output streams and more. 
+use std::io;
+
 // Every player will have a name and a score
 struct Player {
     name: String,
@@ -24,6 +29,71 @@ impl Printable for Player {
 
 }
 
+// This is "collect input" function,
+// will collect the user's input from the command line.
+
+// In order to work, this function takes a single arguement "prompt",
+// which is a string slice.
+// This will be used to prompt the user for input.
+
+fn collect_input<T: std::str::FromStr>(prompt: &str) -> T {
+
+    // This loop will continue to run until the user enters valid information.
+    loop {
+        println!("{}", prompt);
+        
+        // A variable called "input" is created ahead of time,
+        // to store the user's input.
+        // Since the user has not entered their input yet, 
+        // this variable is set to an empty String.
+        // That is what "String::new()" means
+        // The keyword "mut" means mutable.
+        // Mutable means that the value of "input" can be changed.
+        // This makes sense because when the user enters the input,
+        // this will replace the empty String.
+        let mut input = String::new();
+
+        // Read the user's input and modify the variable "input",
+        // without taking ownership of the variable
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read input");
+        
+        // .trim() Remove any whitespace from the input,
+        // .parse() Attempt to convert the trimmed string into another type.
+        // In this context, type T
+        match input.trim().parse() {
+            Ok(value) => return value,
+            Err(_) => continue,
+        }
+
+    }
+
+}
+
+
+
+// NOTE: Ownership and references in Rust
+
+// The default behavior of Rust,
+// is that functions and methods,
+// take ownership of variables that are passed into them.
+
+// To override this behavior, you need to explicitly tell Rust, 
+// that you only want to reference the variable,
+// in the task that you are trying to perform.
+
+// 1. .read_line(input) means that .read_line,
+// will take ownership of the variable.
+
+// 2. .read_line(&input) means that .read_line,
+// will use a read-only copy of the variable.
+
+// 3.  .read_line(&mut input) means that .read_line,
+// will be allowed to modify the variable (mut),
+// BUT the ownership of the variable will NOT be transferred to .readline
+
 fn main() {
     
 }
+
